2b5ec75f6098647ae7fa29fc1e8b7c78c0c95aa7
Okwudili-Daniel01~ <makuochukwuliokwudi@gmail.com>
2023-09-13 16:31:21 +0100

    message

--- stream/http1.js
+++ stream/http1.js
@@ -1,24 +1,48 @@
+// const http = require("http")
+
+// const port = 2000;
+
+// const server = http.createServer((req, res) =>{
+//     if (req.url === "/"){
+//         res.writeHead(200, {"content-type": "text/html"});
+//         res.write(`<html><body><p>This is my home page </p></body></html>`)
+//         res.end()
+//     }else if(req.url === "/about"){
+//         res.writeHead(200, {"content-types":"text/html"});
+//         res.write(`<html><body><p>This is my About page </p></body></html>`)
+//         res.end();
+//     }else if(req.url === "/contact"){
+//         res.writeHead(200, {"content-types": "text/html"})
+//         res.write(`<html><body><p>this is my contact page </p></body></html>`)
+//         res.end();
+//     }else res.end("Invalid Request")
+// })
+
+// server.listen(port, () =>{
+//     console.log("");
+//     console.log("Done");
+// })
+
 const http = require("http")
 
-const port = 2000;
+const port = 2000
 
 const server = http.createServer((req, res) =>{
-    if (req.url === "/"){
-        res.writeHead(200, {"content-type": "text/html"});
-        res.write(`<html><body><p>This is my home page </p></body></html>`)
-        res.end()
-    }else if(req.url === "/about"){
+    if (req === "/"){
+        res.writeHead(200, {"content-types": "text/html"});
+        res.write(`<html><body><p> Welcome </p></body></html>`);
+        res.end();
+    }else if(req === "/about"){
         res.writeHead(200, {"content-types":"text/html"});
-        res.write(`<html><body><p>This is my About page </p></body></html>`)
+        res.write(`<html><body><p> welcome </p></body></html>`);
         res.end();
-    }else if(req.url === "/contact"){
-        res.writeHead(200, {"content-types": "text/html"})
-        res.write(`<html><body><p>this is my contact page </p></body></html>`)
-        res.end();
-    }else res.end("Invalid Request")
+    }else if(req === "/contact"){
+        res.writeHead(200, {"content-types":"text/html"})
+        res.write(`<html><body><p> Welcome </p></body></html>`)
+        res.end()
+    }else res.end("wrong")
 })
 
 server.listen(port, () =>{
-    console.log("");
-    console.log("Done");
+    console.log("Done")
 })
--- stream/http1.ts
+++ stream/http1.ts
@@ -24,8 +24,8 @@ const port: number = 4000;
 //     },
 // ]
 
-const server = http.createServer((req:IncomingMessage, res:ServerResponse<IncomingMessage>) =>{
-    res.writeHead(200);
+// const server = http.createServer((req:IncomingMessage, res:ServerResponse<IncomingMessage>) =>{
+//     res.writeHead(200);
 
     // if(req.url === "/" && req.method === "GET" && res.statusCode === 200) {
     //     res.setHeader("Content-Type", "application/json");
@@ -34,41 +34,83 @@ const server = http.createServer((req:IncomingMessage, res:ServerResponse<Incomi
     // }
     //  res.end();
 
-    let connect: string = "../site/";
+//     let connect: string = "../site/";
 
-    switch (req.url){
+//     switch (req.url){
+//         case "/":
+//             connect += "home.html";
+//             res.statusCode = 200;
+//             break;
+//         case "/about":
+//         connect +="about.html";
+//         res.statusCode = 200;
+//         break;
+//     case "/contact":
+//     connect += "contact.html";
+//     res.statusCode = 200;
+//     break;
+//     default:
+//         connect += "404.html";
+//         res.statusCode = 404;
+//         break;
+//     }
+
+//     fs.readFile(path.join(__dirname, connect), (err, data) =>{
+//         if (err) {
+//             console.log("An erro occured", err);
+//             res.end();            
+//         } else{
+//             res.write(data);
+//             res.end();
+//         }
+//     })
+// })
+
+// server.listen(port, () =>{
+//     console.log("");
+//     console.log("server is listening to port on port", port);
+    
+    
+// })
+
+const server = http.createServer((req:IncomingMessage, res:ServerResponse<IncomingMessage>) =>{
+    res.writeHead(200)
+
+    let connect: string = "../sites"
+
+    switch(req.url){
         case "/":
             connect += "home.html";
             res.statusCode = 200;
             break;
+        case "/contact":
+            connect += "contact.html"
+            res.statusCode = 200
+            break;
         case "/about":
-        connect +="about.html";
-        res.statusCode = 200;
-        break;
-    case "/contact":
-    connect += "contact.html";
-    res.statusCode = 200;
-    break;
-    default:
-        connect += "404.html";
-        res.statusCode = 404;
-        break;
+            connect += "about.html"
+            res.statusCode = 200
+            break;
+        default:
+            connect += "404.html"
+            res.statusCode = 404
+            break;
+
     }
 
-    fs.readFile(path.join(__dirname, connect), (err, data) =>{
-        if (err) {
-            console.log("An erro occured", err);
-            res.end();            
-        } else{
-            res.write(data);
-            res.end();
-        }
-    })
+        fs.readFile(path.join(__dirname, connect), (err, data) =>{
+            if (err){
+                console.log("An erro in creation", err); 
+                res.end()           
+            }else{
+                res.write(data)
+                res.end
+            }
+        })
+    
 })
 
 server.listen(port, () =>{
-    console.log("");
-    console.log("server is listening to port on port", port);
-    
+    console.log("Done");
     
 })
